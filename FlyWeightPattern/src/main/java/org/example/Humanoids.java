f56a5ab2ff2aae159cd891e2f9b197d06bf66bbe
Sher <shersin9891@gmail.com>
2024-03-16 16:18:36 +0530

    Flu weight design pattern

---
+++ FlyWeightPattern/src/main/java/org/example/Humanoids.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Homonoids implements Irobot{
+
+    private String type;
+
+    private Spirit spirit;
+
+    public Homonoids(String type, Spirit spirit){
+        this.type = type;
+        this.spirit = spirit;
+    }
+
+    @Override
+    public void display(int x, int j) {
+
+    }
+}
